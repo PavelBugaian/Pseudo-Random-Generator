4207a57d62521fb64db1f40c89d02e97cfbb1cf9
beater13 <tims85718@gmail.com>
2018-09-20 14:05:49 +0300

    Finished program with 2 files

--- 9_20.c
+++ 9_20.c
@@ -1,53 +1,69 @@
-  #include <stdlib.h>
-  #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-  int count;
 
-  int main() {
+FILE *f, *g;
 
-      int x;
 
-      printf("Write initial 4-digit number: ");
-      scanf("%d", &x);
+int count;
 
-      generator( x );
+int main() {
 
-      return 0;
-    }
+    f = fopen("random1.txt", "w");
+    g = fopen("secret.txt", "w");
 
-  int generator( long seed ) {
+    int x;
 
-      for( int i = 1; i <= 101; ++i ) {
+    printf("Write initial 4-digit number: ");
+    scanf("%d", &x);
 
-          seed *= seed;
+    generator( x );
 
-          int seedCount = counter( seed );
-          if( seedCount > 7 ) {
+    fclose(f);
+    fclose(g);
 
-              seed /= 10;
-          }
+    return 0;
+  }
 
-          seed = (seed / 100) % 10000;
+int generator( long seed ) {
 
-          seedCount = counter( seed );
-          if( seedCount < 4 ){
+    for( int i = 1; i <= 101; ++i ) {
+
+            seed *= seed;
+
+            int seedCount = counter( seed );
+            if( seedCount > 7 ) {
+
+                seed /= 10;
+            }
+
+            seed = (seed / 100) % 10000;
+
+            seedCount = counter( seed );
+            if( seedCount < 4 ){
 
               seed = seed * 10 + 7;
-          }
+            }
 
-          printf("Number: %d\n", seed);
+        if( i < 101 ) {
+
+        fprintf(f ,"Number: %d\n", seed);
+      } else {
+        fprintf(g ,"Number: %d\n", seed);
+
       }
   }
+}
 
-  int counter( long seedToCount ) {
+int counter( long seedToCount ) {
 
-    count = 0;
+  count = 0;
 
-    while( seedToCount > 0 ) {               //Count number of the seed's digits
+  while( seedToCount > 0 ) {               //Count number of the seed's digits
 
-        seedToCount = seedToCount / 10;
-        count++;
-    }
-
-    return count;
+      seedToCount = seedToCount / 10;
+      count++;
   }
+
+  return count;
+}
