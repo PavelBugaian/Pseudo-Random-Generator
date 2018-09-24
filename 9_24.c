851d655feb7f24525345a8f138191c28af6acfe5
TheTrueIsten <rusnak.dan@mail.ru>
2018-09-24 14:59:10 +0300

    Edited. Excluded if's.

--- 9_24.c
+++ 9_24.c
@@ -1,30 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
-int a,i,seed,seed_1,counter;
-FILE *random, *secret;
-int randomer (seed) {
-for(i=0;i<=101;i++) {
-seed*=seed;
 
-seed=seed / 100 %10000;
-printf(" %d \n",seed);
-counter++;
-if (counter<=100){
-fprintf(random,"%d \n",seed);
+int a, b = 10000, i, seed;
+FILE *random, *secret;
+
+void randomer (seed) {
+    for (i = 0; i <= 101; i++) {
+        seed*=seed;
+        seed=seed / 100 % b;
+        fprintf (random,"%d \n", seed);
+        seed = seed + 139;
+    }
+
+    seed *= seed;
+    seed = seed / 100 % 10000;
+    fprintf (secret,"%d", seed);
 }
-if(counter==101){
-fprintf(secret,"%d", seed);
-}
-seed=seed+139;
-}
-}
-int main() {
-random=fopen("numbers.txt","w");
-secret=fopen("secret.txt","w");
-printf(" Enter 4-digit number: ");
-scanf("%d",&seed);
-randomer (seed);
-fclose(random);
-fclose(secret);
-return 0;
+
+void main() {
+    random = fopen ("numbers.txt","w");
+    secret = fopen ("secret.txt","w");
+
+    printf ("Enter 4-digit number: ");
+    scanf ("%d", &seed);
+
+    randomer (seed);
+
+    fclose (random);
+    fclose (secret);
 }
